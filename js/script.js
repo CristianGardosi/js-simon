d42e2591d12dfd45ba96b4ba9105725b6da819cb
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-10-29 17:24:32 +0100

    !!! CONTINUARE DA QUA

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@
 $(document).ready( function() {
     // Array che racchiude i 5 numeri casuali generati dal computer, verrà riempito attraverso la generazione degli stessi nel ciclo for
     var numbers = [];
+    // Array che racchiude i numeri corretti che l'utente inserisce quando gli viene chiesto quali cifre ricorda
+    var rightNumbers = [];
     // Genero i 5 numeri casuali che l'utente deve ricordare; li stampo nell'alert su schermo e li aggiungo nell'array vuoto che ho creato in precendenza
     for (var i = 0; i < 5; i++) {
         var  numeriComputer = randomNumbers(1, 100);
@@ -20,22 +22,21 @@ $(document).ready( function() {
 
     // COUNTDOWN
     var display = $('.countdown');
-    var seconds = 30;
+    var seconds = 1;
 
     var countdown30Sec = setInterval( function() {
         if (seconds === 0) {
             display.text('Tempo scaduto! Ricordi i cinque numeri?')
+
+            for (var i = 0; i < numbers.length; i++) {
+                parseInt( prompt('Inserisci, uno per volta, i cinque numeri che ti sono stati detti ad inizio gioco') );
+            }
+
         } else {
             display.text(seconds);
             seconds--;
         }
     }, 1000);
-
-
-
-
-
-
 
 
 });
